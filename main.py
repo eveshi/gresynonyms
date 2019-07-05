8138c5bd53f980ae676cdb1497af3ec1d05f992c
Eve Shi <qianyiwork@gmail.com>
2019-07-05 16:05:14 +0800

    add input words func

---
+++ main.py
@@ -0,0 +1,15 @@
+# 4 modes
+# - search for synonyms
+# - do comments
+# - easy game: synonym quiz
+# - quick memorize: random words memorize
+
+from utils import save_words
+
+def main():
+    input_continue = "y"
+    while input_continue == "y":
+        save_words.input_pairs()
+        input_continue = input("continue input? y/N: ")
+
+main()
---
+++ utils/save_words.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+with open(path + '/../db/words.json') as words:
+    words_dict = words_dict = json.load(words)
+with open(path + '/../db/explainations.json') as explainations:
+    explainations_dict = json.load(explainations)
+
+def input_pairs():
+    explainations_input = input("the explaination is: ")
+    words_input = input("words(use space between different words): ")
+
+    words_list = words_input.split(" ")
+
+    for word in words_list:
+        if (word in words_dict) and (explainations_input not in words_dict[word]):
+            words_dict[word] = list(set([explainations_input] + words_dict[word]))
+        elif (word in words_dict) and (explainations_input in words_dict[word]):
+            print("duplicate inputs")
+        else:
+            words_dict[word] = [explainations_input]
+
+    if explainations_input in explainations_dict:
+        explainations_dict[explainations_input] = list(set(words_list + explainations_dict[explainations_input]))
+    else:
+        explainations_dict[explainations_input] = words_list
+
+    with open(path + '/../db/words.json', 'w') as words:
+        json.dump(words_dict, words, indent=2, ensure_ascii=False)
+    with open(path + '/../db/explainations.json', 'w') as explainations:
+        json.dump(explainations_dict, explainations, indent=2, ensure_ascii=False)
+
+    print("save successfully")
+
+    
